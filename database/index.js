56fd7536e08e7f23ca7be5d2330982f29083bcf0
Wylie Zhao <wylie@Wylies-Air.fios-router.home>
2021-07-31 18:36:15 -0400

    complete Postgres database functions

--- database/index.js
+++ database/index.js
@@ -1,53 +1,75 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Information', {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
+const { Pool } = require('pg');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env')});
 
-db.on('error', console.error.bind(console, 'Error Connecting Database'));
-db.once('open', function() {
-  console.log('Database Connected');
-});
+const pool = new Pool();
 
-const fakeSchema = new mongoose.Schema({
-  productId: {
-    type: String,
-    unique: true
-  },
-  aspectRatio: String,
-  rating: String,
-  dimensions: String,
-  format: String,
-  runTime: String,
-  releaseDate: Date,
-  cast: Array,
-  studio: String,
-  numberOfDisks: Number,
-});
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
 
+// CREATE
+productCreate = (product) => {
+  return pool
+    .query(`INSERT INTO PRODUCT (ASPECT_RATIO, RATING_ID, DIMENSIONS, FORMAT_ID, RUNTIME, RELEASE_DATE, NUMBER_OF_DISKS, CAST_LIST, STUDIO_ID) VALUES ('${product.ASPECT_RATIO}', ${product.RATING_ID}, '${product.DIMENSIONS}', ${product.FORMAT_ID}, '${product.RUNTIME}', '${product.RELEASE_DATE}', ${product.NUMBER_OF_DISKS}, '${JSON.stringify({ data: product.CAST_LIST })}', ${product.STUDIO_ID})`)
+    .then(res => {
+      return Promise.resolve({ success: `CREATE ${res.rowCount}` })
+    })
+    .catch(err => {
+      return Promise.resolve({ error: err.stack })
+    })
+}
 
-let Information = mongoose.model('Information', fakeSchema);
+// READ
+productRead = (product_id) => {
+  return pool
+    .query(`SELECT PRODUCT_ID, ASPECT_RATIO, RATING_NAME, DIMENSIONS, FORMAT_NAME, RUNTIME, RELEASE_DATE, NUMBER_OF_DISKS, CAST_LIST, STUDIO_NAME FROM PRODUCT A INNER JOIN RATING B ON A.RATING_ID = B.RATING_ID INNER JOIN FORMAT C ON A.FORMAT_ID = C.FORMAT_ID INNER JOIN STUDIO D ON A.STUDIO_ID = D.STUDIO_ID WHERE PRODUCT_ID = ${product_id}`)
+    .then(res => {
+      if (res.rowCount === 0) {
+        throw { stack: 'Invalid PRODUCT_ID' }
+      }
+      console.log(res)
+      return Promise.resolve(res.rows[0])
+    })
+    .catch(err => {
+      return Promise.resolve({ error: err.stack })
+    })
+}
 
-let returnData = (id) => {
-  return Information.findOne({productId: id}).exec();
-};
+// UPDATE
+productUpdate = (product) => {
+  return pool
+    .query(`UPDATE PRODUCT SET ASPECT_RATIO = '${product.ASPECT_RATIO}', RATING_ID = ${product.RATING_ID}, DIMENSIONS = '${product.DIMENSIONS}', FORMAT_ID = ${product.FORMAT_ID}, RUNTIME = '${product.RUNTIME}', RELEASE_DATE = '${product.RELEASE_DATE}', NUMBER_OF_DISKS = ${product.NUMBER_OF_DISKS}, CAST_LIST = jsonb_set(CAST_LIST, '{data}', '${JSON.stringify(product.CAST_LIST)}'), STUDIO_ID = ${product.STUDIO_ID} WHERE PRODUCT_ID = ${product.PRODUCT_ID}`)
+    .then(res => {
+      if (res.rowCount === 0) {
+        throw { stack: 'Invalid PRODUCT_ID' }
+      }
+      return Promise.resolve({ success: `UPDATE ${res.rowCount}` })
+    })
+    .catch(err => {
+      return Promise.resolve({ error: err.stack })
+    })
+}
 
-let createInformation = (information) => {
-  return Information.insertMany([information]).exec();
-};
+// DELETE
+productDelete = (product_id) => {
+  return pool
+    .query(`DELETE FROM PRODUCT WHERE PRODUCT_ID = ${product_id}`)
+    .then(res => {
+      if (res.rowCount === 0) {
+        throw { stack: 'Invalid PRODUCT_ID' }
+      }
+      return Promise.resolve({ success: `DELETE ${res.rowCount}`})
+    })
+    .catch(err => {
+      return Promise.resolve({ error: err.stack })
+    })
+}
 
- let updateInformation = (id, information) => {
-  return Information.findOneAndUpdate({productId: id}, {
-    $set: information
-  }).exec();
-};
-
-let deleteInformation = (id) => {
-  return Information.deleteOne({
-    productId: id
-  }).exec();
-};
-
-module.exports.returnData = returnData;
-module.exports.Information = Information;
-module.exports.createInformation = createInformation;
-module.exports.updateInformation = updateInformation;
-module.exports.deleteInformation = deleteInformation;
+module.exports = {
+  productCreate,
+  productRead,
+  productUpdate,
+  productDelete
+}
